bd580057c228520a1220ac99873660e91f95dfc2
googlakk <googl.akk@mail.ru>
2024-01-21 16:38:03 +0600

    second commut

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import Routing from "@pages";
 import { withProviders } from "./providers";
 const App: React.FC = () => <Routing />;
-// eslint-disable-next-line react-refresh/only-export-components
+
 export default withProviders(App);
